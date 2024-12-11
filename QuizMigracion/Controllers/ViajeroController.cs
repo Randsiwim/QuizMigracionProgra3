1f65862c1d66d57e5a428fe9771c70a939e7a4aa
Randsiwim <randsiwin13@gmail.com>
2024-12-10 20:59:29 -0600

    2

---
+++ QuizMigracion/Controllers/ViajeroController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControlMigracion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlMigracion.Controllers
+{
+    public class ViajerosController : Controller
+    {
+        private readonly DbContext _context;
+
+        public ViajerosController(DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Viajeros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Viajeros/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Viajero viajero)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(viajero);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viajero);
+        }
+
+        // GET: Viajeros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Viajeros.ToListAsync());
+        }
+
+        // GET: Viajeros/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var viajero = await _context.Viajeros
+                .FirstOrDefaultAsync(m => m.ViajeroID == id);
+            if (viajero == null)
+            {
+                return NotFound();
+            }
+
+            return View(viajero);
+        }
+    }
+}
--- QuizMigracion/Models/LoginViewModel.cs
+++ QuizMigracion/Models/LoginViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace ControlMigracion.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlMigracion.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
         public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
         public string Contraseña { get; set; }
     }
 }
